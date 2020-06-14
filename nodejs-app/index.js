6b07eedea2b8305c2d271c837b62f34c719816d9
Karthik Kumar <jkkr.1996@gmail.com>
2020-06-14 22:44:20 +0530

    feat: Add NodeJs docker app

---
+++ nodejs-app/index.js
@@ -0,0 +1,14 @@
+"use strict";
+const koa = require("koa");
+const koaRouter = require("koa-router");
+
+const app = new koa();
+const router = new koaRouter();
+
+router.get("koala", "/", (ctx) => {
+  ctx.body = "Welcome! To the Koala Book of Everything!";
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(1234, () => console.log("running on port 1234"));
